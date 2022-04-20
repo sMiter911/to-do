ff94e5c3921acc702cf7cde628bc9f7ed577beac
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-04-20 17:21:13 +0200

    WIP: Creation static homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [
+    {
+      itemName: 'Coding',
+      itemDueDate: '2022-04-05',
+      itemPriority: 'high',
+      itemCategory: 'Work',
+    },
+    {
+      itemName: 'Assignment',
+      itemDueDate: '2022-05-01',
+      itemPriority: 'medium',
+      itemCategory: 'School',
+    },
+    {
+      itemName: 'Shopping',
+      itemDueDate: '2022-04-27',
+      itemPriority: 'low',
+      itemCategory: 'Personal',
+    },
+    {
+      itemName: 'Gym',
+      itemDueDate: '2022-04-27',
+      itemPriority: 'low',
+      itemCategory: 'Personal',
+    },
+  ];
+
+  today: number = Date.now();
 
   constructor() {}
-
 }
